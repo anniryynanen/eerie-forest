bbdcf14b5adc091df12b515b313a0236187a2cc2
Anni Ryynänen <anni@anniryynanen.com>
2024-05-19 18:14:50 +0300

    Menu button

--- src/main.gd
+++ src/main.gd
@@ -4,3 +4,6 @@ func _enter_tree() -> void:
     get_window().min_size = Vector2i(
         ProjectSettings.get_setting("display/window/size/viewport_width"),
         ProjectSettings.get_setting("display/window/size/viewport_height"))
+
+func _on_menu_button_pressed() -> void:
+    print("menu!")
